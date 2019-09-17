2634de79efc671794534523db65dba9ebc9467c9
swsoyee <swsoyee@gmail.com>
2019-09-17 17:08:03 +0900

    完成推荐列表组件

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,18 +1,29 @@
 //src/application/Recommend/index.js
 
 import React from 'react';
-import Slider from '../../components/slider'
+import Slider from '../../components/slider';
+import RecommendList from '../../components/list/list';
 
 function Recommend() {
 
     // mock data
-    const bannerList = [1, 2, 3, 4].map(item => {
+    const bannerList = [1, 2, 3, 4].map(() => {
         return {imageUrl: "http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg"}
+    })
+
+    const recommendList = [...Array(10).keys()].map(item => {
+        return {
+            id: 1,
+            picUrl: "https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg",
+            playCount: 114514,
+            name: "朴树、许巍、李健、郑钧、老狼、赵雷"
+        }
     })
 
     return (
         <div>
             <Slider bannerList={bannerList}></Slider>
+            <RecommendList recommendList={recommendList}></RecommendList>
         </div>
     )
 }
